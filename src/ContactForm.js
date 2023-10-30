26f8612d8fd8838dcae036bf2c0ae9b18e7d8a1b
Wojciach <wojciech.wyroslak@gmail.com>
2023-10-30 12:55:16 +0100

    composer for phpmailer added and mysql request fix

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -11,6 +11,7 @@ function ContactForm() {
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
+        console.log(formData);
 
         fetch('http://localhost:80/my-react-app/public/php/sendForm.php', {
             mode: 'cors',
@@ -44,10 +45,10 @@ function ContactForm() {
             <div className="fake-margin" >
                 <h2>INTERESTED?</h2>
                 <form onSubmit={handleSubmit}>
-                    <input id="rName" name="rName" type="text" placeholder="Name & Surname..." required maxLength="50"/>
-                    <input id="rPhone" name="rPhone" type="tel" placeholder="Telephone..." required maxLength="20" pattern="^\+?(?:[0-9] ?){6,14}[0-9]$"/>
-                    <input id="rEmail" name="rEmail" type="email" placeholder="Email..." required maxLength="100"/>
-                    <textarea id="rMsg" name="rMsg" rows="130" placeholder="Tell me more..." required maxLength="1000"></textarea>
+                    <input id="rName" name="name" type="text" placeholder="Name & Surname..." required maxLength="50"/>
+                    <input id="rPhone" name="phone" type="tel" placeholder="Telephone..." required maxLength="20" pattern="^\+?(?:[0-9] ?){6,14}[0-9]$"/>
+                    <input id="rEmail" name="email" type="email" placeholder="Email..." required maxLength="100"/>
+                    <textarea id="rMsg" name="message" rows="130" placeholder="Tell me more..." required maxLength="1000"></textarea>
                     <button type="submit">send<span className="material-symbols-rounded">trending_flat</span></button> 
                 </form>
             </div>
