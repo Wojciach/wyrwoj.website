608af22dbcdd64b422fb48471f43f45788076950
Wojciach <wojciech.wyroslak@gmail.com>
2023-11-01 11:57:18 +0100

    repository renamed

--- public/php/sendForm.php
+++ public/php/sendForm.php
@@ -13,7 +13,7 @@ use Wojciach\Wojciach\PHPMailerEmail;
 use Wojciach\Wojciach\RequestDatabase;
 use Wojciach\Wojciach\EmailBody;
 
-$RequestDatabase = new RequestDatabase("db_passProd.php");
+$RequestDatabase = new RequestDatabase("db_passDev.php");
 
 if($RequestDatabase->howManyMessages() >= 5) {
   echo 'tooMany';
